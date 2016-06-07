d7466c50e3737da5bc1801085a0673908aef0eeb
Sajjon <alex.cyon@gmail.com>
2016-06-07 12:42:47 +0200

    Minimum Project show that multiple post_install is not supported and not warned about

---
+++ PodBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodBug
+//
+//  Created by Cyon Alexander (Ext. Netlight) on 07/06/16.
+//  Copyright © 2016 com.alexcyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+
+
+target 'PodBug' do
+	
+end
+
+post_install do |installer|
+	puts "1️⃣ 1️⃣ 1️⃣  First post_install, should see print from second post_install below ⬇️ ⬇️ ⬇️"
+end
+
+post_install do |installer|
+	puts "2️⃣ 2️⃣ 2️⃣️  Second and last post_install, should see print from first post_install above this print ⬆️ ⬆️ ⬆️"
+end
+
